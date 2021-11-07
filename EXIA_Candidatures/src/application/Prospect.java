1da64b78d22f59f73aca564546bb9c84716e0f87
ALIANE <ALIANE@COPR41.CESI-ALGERIE.PRIVE>
2021-11-07 14:17:11 +0100

    prospects refonte

--- EXIA_Candidatures/src/application/Prospect.java
+++ EXIA_Candidatures/src/application/Prospect.java
@@ -19,6 +19,93 @@ public class Prospect {
 		
 	}
 	
+	/*******************getters*****************/
+	
+	public int getIdProspect () {
+		return idProspect;
+	}
+	
+	public int getTel1 () {
+		return tel1;
+	}
+
+	public int getTel2 () {
+		return tel2;
+	}
+	
+	public String getNom () {
+		return nom;
+	}
+	
+	public String getPrenom () {
+		return prenom;
+	}
+	
+    public String getBac () {
+		return bac;
+	}
+    
+    public String getNivEtude () {
+		return niv_etude;
+	}
+    
+    public Date getDateNaissance() {
+		return dateNaissance;
+	}
+    
+    public String getEmail () {
+		return email;
+	}
+    
+    public String getRemarque() {
+		return remarque;
+	}
+    
+    /******************Setters*******************/
+    
+    public void setIdProspect(int idProspect) {
+		this.idProspect = idProspect;
+	}
+    
+    
+    public void setTel2(int tel2) {
+		this.tel2 = tel2;
+	}
+    
+    public void setTel1(int tel1) {
+		this.tel1 = tel1;
+	}
+    
+    public void setNom(String nom) {
+		this.nom = nom;
+	}
+    
+    public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+    
+    public void setBac(String bac) {
+		this.bac = bac;
+	}
+    
+    public void setNivEtude(String niv_etude) {
+		this.niv_etude = niv_etude;
+	}
+    
+    public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+    
+    public void setEmail(String email) {
+		this.email = email;
+	}
+    
+	public void setRemarque(String remarque) {
+		this.remarque = remarque;
+	}
+   
+	/******************Methods****************/
+    
 	public void RechercherProspect() {
 		
 	}
@@ -30,5 +117,9 @@ public class Prospect {
 	public void SupprimerProspect() {
 		
 	}
+
+	
+
+	
 }
 
